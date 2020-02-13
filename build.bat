3e770c220eb05618022db0edcdfae073b3412357
mmrwizard <1292252803@qq.com>
2020-02-13 13:50:10 +0800

    update

--- build.bat
+++ build.bat
@@ -1,20 +1,20 @@
 mkdir build
 cd build
 
-set UNIX_PREFIX=%CONDA_PREFIX:%
-set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:%
-set UNIX_LIBRARY_BIN=%LIBRARY_BIN:%
-set UNIX_LIBRARY_INC=%LIBRARY_INC:%
-set UNIX_LIBRARY_LIB=%LIBRARY_LIB:%
-set UNIX_SP_DIR=%SP_DIR:%
-set UNIX_SRC_DIR=%SRC_DIR:%
+set UNIX_PREFIX=%CONDA_PREFIX:\=/%
+set UNIX_LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
+set UNIX_LIBRARY_BIN=%LIBRARY_BIN:\=/%
+set UNIX_LIBRARY_INC=%LIBRARY_INC:\=/%
+set UNIX_LIBRARY_LIB=%LIBRARY_LIB:\=/%
+set UNIX_SP_DIR=%SP_DIR:\=/%
+set UNIX_SRC_DIR=%SRC_DIR:\=/%
 
-pushd  "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+pushd %VS2017INSTALLDIR%
 call VC\Auxiliary\Build\vcvars64.bat
 popd
 
 cmake -G "Visual Studio 15 2017" -A "x64" -T "host=x64" ^
-    -DCMAKE_TOOLCHAIN_FILE="G:\wangzd\projects\renderMatch\RenderMatch\toolchains\h2o.cmake" ^
+    -DCMAKE_TOOLCHAIN_FILE=%UNIX_PREFIX%/h2o.cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=./install ^
     -DBUILD_SHARED_LIBS=ON ^
